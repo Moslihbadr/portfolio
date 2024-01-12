f99c516dada6097682bf7437a26e5bd32e4b0357
Badr <moslihbadr2@gmail.com>
2024-01-12 21:46:28 +0000

    update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,14 +25,14 @@ const Hero = () => {
           {language === "en" ? <h1>Hi, I&lsquo;m <br />Badr Moslih 👋</h1> : <h1>Salut, je suis<br /> Badr Moslih 👋</h1>}
           <StyledAboutMe>
             {language === "en" ?
-              <p className="mt-3 mb-4"><span style={{ color: "var(--main-green)" }}>&lt;Full-stack web developer/&gt;</span> passionate about programming.</p>
-              : <p className="mt-3 mb-4"> <span style={{ color: "var(--main-green)" }}>&lt;Développeur web full-stack/&gt;</span> passionné par la programmation.</p>
+              <p className="mt-3 mb-4"><span style={{ color: "var(--main-green)" }}>&lt;Full-stack web developer /&gt;</span> passionate about programming.</p>
+              : <p className="mt-3 mb-4"> <span style={{ color: "var(--main-green)" }}>&lt;Développeur web full-stack /&gt;</span> passionné par la programmation.</p>
             }
           </StyledAboutMe>
           <a href="mailto:moslihbadr2@gmail.com"><Button>{language === 'en' ? 'Get in touch' : 'Contactez-moi'}</Button></a>
         </div>
         <div className="d-none d-lg-block">
-          <img src="../../assets/hero.png" style={{transform: 'translateX(25px)'}} width={450} height={450} />
+          <img src="hero.png" alt="Hero" style={{transform: 'translateX(25px)'}} width={450} height={450} />
         </div>
       </div>
     </StyledHero>
