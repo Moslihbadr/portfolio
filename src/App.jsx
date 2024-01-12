8198859a113904fd49597dbd74d6c8abe6e4ef18
Badr <moslihbadr2@gmail.com>
2024-01-12 21:18:51 +0000

    render components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import About from "./components/About"
+import Hero from "./components/Hero"
+import Navbar from "./components/navbar/Navbar"
+import { LanguageProvider } from "./contexts/LanguageContext"
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <LanguageProvider>
+      <div className="container">
+        <Navbar />
+        <Hero />
+        <About />
+      </div>
+    </LanguageProvider>
   )
 }
 
