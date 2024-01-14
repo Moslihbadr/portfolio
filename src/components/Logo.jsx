39cf02855aaf66a9df5c422c1b382d04d0e58e0f
Badr <moslihbadr2@gmail.com>
2024-01-14 23:11:47 +0000

    update Logo component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,9 @@
 const Logo = ({ color }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="120" height="55" style={{ fontWeight: 'bold', fontSize: '25px' }}>
-      <text x="0" y="50" style={{transform: 'translateY(-13px)'}} fill="#00BB67" letterSpacing="0.1em" fontFamily="Arial, sans-serif">{"<B"}</text>
-      <text x="35" y="50" style={{transform: 'translateY(-13px)'}} fill={color} fontFamily="Arial, sans-serif">adr</text>
-      <text x="85" y="50" style={{transform: 'translateY(-13px)'}} fill="#00BB67" letterSpacing="0.1em" fontFamily="Arial, sans-serif">{"/>"}</text>
+    <svg xmlns="http://www.w3.org/2000/svg" width="120" height="55" style={{ fontWeight: '500', fontSize: '25px' }}>
+      <text x="0" y="50" style={{transform: 'translateY(-13px)'}} fill="#00BB67" letterSpacing="0.1em" fontFamily= 'Poppins'>{"<B"}</text>
+      <text x="35" y="50" style={{transform: 'translateY(-13px)'}} fill={color} fontFamily= 'Poppins'>adr</text>
+      <text x="85" y="50" style={{transform: 'translateY(-13px)'}} fill="#00BB67" letterSpacing="0.1em" fontFamily= 'Poppins'>{"/>"}</text>
     </svg>
   );
 };
